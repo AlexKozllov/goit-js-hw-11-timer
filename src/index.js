cf30c4d36e249baf6e327cb57e4eb827101b81f5
Alex_Kozzlov <alexandr.kozlov1120@gmail.com>
2020-10-04 22:53:12 +0300

    added hw11 task-3 Timer

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import css from "./css/style.css";
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.days = document.querySelector(selector);
+    this.targetDate = targetDate;
+    this.interval = "";
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  getDays(time) {
+    return this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  }
+  getHours(time) {
+    return this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+  }
+  getMins(time) {
+    return this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  }
+  getSecs(time) {
+    return this.pad(Math.floor((time % (1000 * 60)) / 1000));
+  }
+  updateClockface(time) {
+    if (time) {
+      this.days.children[0].children[0].textContent = this.getDays(time);
+      this.days.children[1].children[0].textContent = this.getHours(time);
+      this.days.children[2].children[0].textContent = this.getMins(time);
+      this.days.children[3].children[0].textContent = this.getSecs(time);
+    }
+  }
+  start() {
+    this.interval = setInterval(() => {
+      this.deltaTime = this.targetDate - Date.now();
+      this.updateClockface(this.deltaTime);
+    }, 1000);
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Jan 1 2021"),
+});
+
+timer.start();
